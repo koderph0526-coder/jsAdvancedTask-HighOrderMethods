39a917087b80b4b505b8fc6d1776a54be90e2651
koderph0526-coder <koderph0526@gmail.com>
2026-09-02 14:32:42 +0200

    UpdatedCommentsAndAddedExplanationOfWhyChosenMehtods

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Assignment Source link from Canvas:
 'https://jobloop.instructure.com/courses/557/assignments/11015'
 
 ## My own notes if need be:
+
+- Need to practise appending created JS elements, so I'm attemting to do that below the main task part of the code.
+- An idea to better achieve the wanted result: Make smaller divs to plave the names of each item in products into and repeat fro the other items I'd like to display in the fitting order and style. Match up with styled created for said divs.
--- main.js
+++ main.js
@@ -26,10 +26,12 @@ console.log(
 );
 
 //2: Fetching only the product names, to get a quick overview:
+//Using .map to filter through all the information in the object-array(array-object? help..), as this doesn't alter the original but creates a new array based on the information I ask for.
 const allProducts = products.map((item) => item.name);
 console.log(allProducts);
 
 // 3: Fething only the names from the items under the category of Electronics
+//Using both .filter and then .map, as I want it to only 'map' the catergory of electronics.
 const targetCategory = "electronics";
 const electrnsNames = products
   .filter((product) => product.category === targetCategory)
@@ -38,32 +40,61 @@ const electrnsNames = products
 console.log(electrnsNames);
 
 //4: Checking if one or more of the products price is over 1000
-
+//Using .some as this method could scan throught the array and find that there was indeed an item priced above 1000. While .include doesn't seems to be able to read the different objects storen in the array.
 const isOver1000 = products.some((product) => product.price > 1000);
 console.log(isOver1000);
 //Atempted using .include() here, but it returned false? Would love to have a closer look at this in a lesson as google searches so far did not manage to explain why.
 
 //5: Calculate the total sum of all the items in products.
+//Using .reduce with sum as a parameter, making it run a function throught the array to combine the price of all the items, and I'm telling it to start at index 0 to control it's starting poiint.
 const sumTotal = products.reduce((sum, item) => {
   return sum + item.price;
 }, 0);
 console.log(sumTotal);
+//Note: I'm storing everything in variables to make certain I avoid mutating the orinial array, however it may not have been nessacary in part 2 of the task according to what I understand about the .map method
 
 //
-//This code is purely for myself to train more on coding and design
+//This code is purely for myself to train more on coding with js and design
 
 //Starting with the products under 200
-const lowPriceNames = lowToHighPrice.map((item) => item.name).join(", ");
+// const lowPriceNames = lowToHighPrice.map((item) => item.name).join(", ");
 
 const mediumHeader = document.createTextNode("h2");
-const miniHeader1 = document.createTextNode("h3");
-const title = document.createTextNode("p");
+const contextTxt = document.createTextNode("p");
+const titleN = document.createTextNode("p");
+titleN.classList.add("boldTitle");
+const titleP = document.createTextNode("p");
+const titleC = document.createTextNode("p");
+const category = document.createTextNode("p");
 const price = document.createTextNode("p");
-const divCard = document.createElement("div");
-divCard.classList.add("jsCardBox");
 mediumHeader.textContent = "Organizing and managing assitance";
+cardCont.append(mediumHeader);
+// for (let item of lowToHighPrice) {
+// Creating the card for each catergory of organizing/managing improvement, adding class too
+const divCard = document.createElement("article");
+divCard.classList.add("jsCardBox");
+
+//A little header for the items in their orgnized category in the cards
+const miniHeader1 = document.createTextNode("h3");
 miniHeader1.textContent =
-  "Sorting from lowest price to highest within the targetted group";
-title.textContent = lowToHighPrice.map((item) => item.name).join(", ");
-divCard.append(title);
-cardCont.appendChild(divCard);
+  "The items under 200, possibly a good list for Impulse buy items";
+
+//Styling div for all the title - names
+const divNames = document.createElement("div");
+divNames.classList.add("divNames");
+
+//The Title for each product
+titleN.textContent = `Name ${lessThan200.name}`;
+
+//The price:
+titleP.textContent = `Price: ${lessThan200.price}`;
+//
+div;
+divNames.append(titleN);
+divCard.append(miniHeader1);
+cardCont.append(divCard);
+// Not working the way I want yet. Idea: Make separate divs with styling to separate name, price and category, append into said divs and then append those divs into the cardDiv?
+// }
+// miniHeader1.textContent =
+//   "Sorting from lowest price to highest within the targetted group";
+// Pushing the divcard To it's container in html
